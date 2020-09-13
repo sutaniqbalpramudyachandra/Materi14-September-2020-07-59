2c50994696425198fd0dd1fabb05398d3b120a3a
Sutan Iqbal Pramudya Chandra <71204100+sutaniqbalpramudyachandra@users.noreply.github.com>
2020-09-13 15:00:56 +0700

    deskrpsi 1

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php 
 
 class hewan{
-	public $jumlah_kaki, $jenis_hewan, $bisa_terbang, $suara
+	public $jumlah_kaki, $jenis_hewan, $bisa_terbang, $bersuara ;
+
+	public function cetakjenishewan(){
+		return $this->jenis_hewan;
+	}
 }
+
+
+$milo = new hewan;
+$milo -> jumlah_kaki = "4" ;
+$milo -> jenis_hewan = "kucing" ;
+$milo -> bisa_terbang = "tidak" ;
+$milo -> bersuara = "meowww" ;
+
+echo "Milo adalah jenis hewan ".$milo-> cetakjenishewan() ;
+echo "<br>";
