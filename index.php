58f922cb18a25be11b2b930b7da2ea0609620783
Sutan Iqbal Pramudya Chandra <71204100+sutaniqbalpramudyachandra@users.noreply.github.com>
2020-09-13 15:14:35 +0700

    kucing

--- index.php
+++ index.php
@@ -4,16 +4,31 @@ class hewan{
 	public $jumlah_kaki, $jenis_hewan, $bisa_terbang, $bersuara ;
 
 	public function cetakjenishewan(){
-		return $this->jenis_hewan;
+		return "Milo adalah jenis hewan ".$this-> jenis_hewan;
 	}
+	public function cetakjumlahkaki(){
+		return $this->jumlah_kaki;
+	}
+	public function cetakterbang(){
+		return $this->bisa_terbang;
+	}
+	public function cetaksuaranya(){
+		return $this->bersuara;
+	}
+
 }
 
 
 $milo = new hewan;
 $milo -> jumlah_kaki = "4" ;
 $milo -> jenis_hewan = "kucing" ;
-$milo -> bisa_terbang = "tidak" ;
+$milo -> bisa_terbang = "tidak bisa terbang" ;
 $milo -> bersuara = "meowww" ;
 
-echo "Milo adalah jenis hewan ".$milo-> cetakjenishewan() ;
+echo $milo-> cetakjenishewan() ;
 echo "<br>";
+echo "Punya kaki sebanyak ".$milo-> cetakjumlahkaki();
+echo "<br>";
+echo $milo-> cetakterbang();
+echo "<br>";
+echo "Suaranya : ".$milo-> cetaksuaranya();
